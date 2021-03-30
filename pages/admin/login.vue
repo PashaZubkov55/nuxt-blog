458eec0fbd0dd6c09ba46e99a7bc66d5f23658a1
pavel <pal5555@mail.ru>
2021-03-30 17:33:04 +0300

    login page

--- pages/admin/login.vue
+++ pages/admin/login.vue
@@ -1,3 +1,73 @@
 <template>
-    
+<el-card
+    shadow="always"
+    :style="{width: '500px'}">
+
+     <el-form 
+      :model="controls" 
+      :rules="rules" 
+      ref="form"
+      @submit.native.prevent="onSubmit"
+      >
+      <h1>Войти в систему</h1>
+  <el-form-item label="Логин" prop="login">
+    <el-input v-model.trim="controls.login" />
+  </el-form-item>
+  <el-form-item label="Пароль" prop="password">
+    <el-input 
+    v-model.trim="controls.password" 
+    resize="none"
+    :rows= "2"
+    type= 'password'
+    >
+    </el-input>
+  </el-form-item>
+<el-form-item>
+    <el-button 
+    type="primary" 
+    round
+    native-type="submit"
+    :loading ="loading"
+    >
+        Войти
+    </el-button>
+   
+    </el-form-item>
+      </el-form>
+</el-card>
 </template>
+
+<script>
+export default {
+    layout: 'empty',
+
+     data() {
+        
+       return {
+            loading: false,
+           controls: {
+               login: '',
+               password: ''
+           },
+            rules: {
+            login: [
+            { required: true, message: 'Введите логин ', trigger: 'blur' },
+          ],
+           
+            password: [
+            { required: true, message: 'Введите пароль', trigger: 'blur' },
+            {min: 6 , message: 'Пароль должен быть не мение 6 символов', trigger: 'blur'}
+          ],
+         
+        }
+
+       }
+   },
+
+   methods:{
+       onSubmit(){
+          console.log('form submit')
+       }
+   }
+}
+</script>
